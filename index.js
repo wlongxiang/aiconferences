bdcdbbe29f9c46a8f70f5b50521a309ce2731482
Benjamin Wang <wlongxiang1119@gmail.com>
2020-06-02 21:54:07 +0200

    add custom search bar

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 $(document).ready(function () {
-    $('#example').DataTable(
+    oTable = $('#example').DataTable(
         {
             // "scrollY": "800px",
             // "scrollCollapse": true,
-            "paging": false
+            "paging": false,
+            // below sdom makes search bar dispear, 
+            'sDom': 't', 
+            // info: false
         }
 
     );
+
+$('#mySearchInput').on('keyup change', function(){
+  oTable.search($(this).val()).draw();
+})
 });

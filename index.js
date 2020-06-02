a2b612df51574c8d43dc4d542410ecb527b2bc7e
Benjamin Wang <wlongxiang1119@gmail.com>
2020-06-02 20:59:23 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('#example').DataTable(
+        {
+            // "scrollY": "800px",
+            // "scrollCollapse": true,
+            "paging": false
+        }
+
+    );
+});
